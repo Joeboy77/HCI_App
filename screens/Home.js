dcf1b5009bb34a03661446f52768699b2d2503d5
Joseph <acheampongjoseph470@gmail.com>
2024-06-11 10:44:57 +0000

    update

---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+
+import { Text, StyleSheet, View } from 'react-native'
+
+export default function Home() {
+ 
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    )
+  
+}
+
+const styles = StyleSheet.create({})

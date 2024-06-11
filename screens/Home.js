bf9ff363ba546213c20ac69b80d8f9fb7a88f3da
Joseph <acheampongjoseph470@gmail.com>
2024-06-11 12:23:40 +0000

    update

--- screens/Home.js
+++ screens/Home.js
@@ -4,91 +4,112 @@ import React, { useState } from 'react'
 import Vector from '../assets/images/Vector.png'
 
 export default function Home() {
-    const [isPressed, setIsPressed] = useState(false)
+    
+  const [pressedStates, setPressedStates] = useState(Array(8).fill(false));
+
+  const handlePressIn = (index) => {
+    const newPressedStates = [...pressedStates];
+    newPressedStates[index] = true;
+    setPressedStates(newPressedStates);
+  };
+
+  const handlePressOut = (index) => {
+    const newPressedStates = [...pressedStates];
+    newPressedStates[index] = false;
+    setPressedStates(newPressedStates);
+  };
     return (
         <>
         <Search />
         <View style={styles.container}>
             <View >
                 <View style={styles.boxMain}>
-                    <Pressable style={[styles.box1, isPressed && styles.box1Pressed ]}
-                        onPressIn={()=> setIsPressed(true)}
-                        onPressOut={()=>setIsPressed(false)}
+                    <Pressable 
+                        style={[styles.box1, pressedStates[0] && styles.box1Pressed]}
+                        onPressIn={() => handlePressIn(0)}
+                        onPressOut={() => handlePressOut(0)}
                         >
-                            <View style={[styles.imageBox, isPressed && styles.imageBoxPressed]}>
+                            <View style={[styles.imageBox, pressedStates[0] && styles.imageBoxPressed]}>
                                 <Image source={Vector} />
                             </View>
-                            <Text style={[styles.lawText, isPressed && styles.lawTextPressed]}>Law</Text>
+                            <Text style={[styles.lawText, pressedStates[0] && styles.lawTextPressed]}>Law</Text>
                     </Pressable>
-                    <Pressable style={[styles.box1, isPressed && styles.box1Pressed ]}
-                        onPressIn={()=> setIsPressed(true)}
-                        onPressOut={()=>setIsPressed(false)}
+                    <Pressable 
+                        style={[styles.box1, pressedStates[1] && styles.box1Pressed]}
+                        onPressIn={() => handlePressIn(1)}
+                        onPressOut={() => handlePressOut(1)}
                         >
-                            <View style={[styles.imageBox, isPressed && styles.imageBoxPressed]}>
+                            <View style={[styles.imageBox, pressedStates[1] && styles.imageBoxPressed]}>
                                 <Image source={Vector} />
                             </View>
-                            <Text style={[styles.lawText, isPressed && styles.lawTextPressed]}>Politics</Text>
-                    </Pressable>
-                </View>
-                <View style={styles.boxMain}>
-                    <Pressable style={[styles.box1, isPressed && styles.box1Pressed ]}
-                        onPressIn={()=> setIsPressed(true)}
-                        onPressOut={()=>setIsPressed(false)}
-                        >
-                            <View style={[styles.imageBox, isPressed && styles.imageBoxPressed]}>
-                                <Image source={Vector} />
-                            </View>
-                            <Text style={[styles.lawText, isPressed && styles.lawTextPressed]}>Geography</Text>
-                    </Pressable>
-                    <Pressable style={[styles.box1, isPressed && styles.box1Pressed ]}
-                        onPressIn={()=> setIsPressed(true)}
-                        onPressOut={()=>setIsPressed(false)}
-                        >
-                            <View style={[styles.imageBox, isPressed && styles.imageBoxPressed]}>
-                                <Image source={Vector} />
-                            </View>
-                            <Text style={[styles.lawText, isPressed && styles.lawTextPressed]}>Computer Science</Text>
+                            <Text style={[styles.lawText, pressedStates[1] && styles.lawTextPressed]}>Politics</Text>
                     </Pressable>
                 </View>
                 <View style={styles.boxMain}>
-                    <Pressable style={[styles.box1, isPressed && styles.box1Pressed ]}
-                        onPressIn={()=> setIsPressed(true)}
-                        onPressOut={()=>setIsPressed(false)}
+                    <Pressable 
+                        style={[styles.box1, pressedStates[2] && styles.box1Pressed]}
+                        onPressIn={() => handlePressIn(2)}
+                        onPressOut={() => handlePressOut(2)}
                         >
-                            <View style={[styles.imageBox, isPressed && styles.imageBoxPressed]}>
+                            <View style={[styles.imageBox, pressedStates[2] && styles.imageBoxPressed]}>
                                 <Image source={Vector} />
                             </View>
-                            <Text style={[styles.lawText, isPressed && styles.lawTextPressed]}>Philosopy</Text>
+                            <Text style={[styles.lawText, pressedStates[2] && styles.lawTextPressed]}>Geography</Text>
                     </Pressable>
-                    <Pressable style={[styles.box1, isPressed && styles.box1Pressed ]}
-                        onPressIn={()=> setIsPressed(true)}
-                        onPressOut={()=>setIsPressed(false)}
+                    <Pressable 
+                        style={[styles.box1, pressedStates[3] && styles.box1Pressed]}
+                        onPressIn={() => handlePressIn(3)}
+                        onPressOut={() => handlePressOut(3)}
                         >
-                            <View style={[styles.imageBox, isPressed && styles.imageBoxPressed]}>
+                            <View style={[styles.imageBox, pressedStates[3] && styles.imageBoxPressed]}>
                                 <Image source={Vector} />
                             </View>
-                            <Text style={[styles.lawText, isPressed && styles.lawTextPressed]}>Languages</Text>
+                            <Text style={[styles.lawText, pressedStates[3] && styles.lawTextPressed]}>Computer Science</Text>
+                    </Pressable>
+                </View>
+                <View style={styles.boxMain}>
+                    <Pressable 
+                        style={[styles.box1, pressedStates[4] && styles.box1Pressed]}
+                        onPressIn={() => handlePressIn(4)}
+                        onPressOut={() => handlePressOut(4)}
+                        >
+                            <View style={[styles.imageBox, pressedStates[4] && styles.imageBoxPressed]}>
+                                <Image source={Vector} />
+                            </View>
+                            <Text style={[styles.lawText, pressedStates[4] && styles.lawTextPressed]}>Philosopy</Text>
+                    </Pressable>
+                    <Pressable 
+                        style={[styles.box1, pressedStates[5] && styles.box1Pressed]}
+                        onPressIn={() => handlePressIn(5)}
+                        onPressOut={() => handlePressOut(5)}
+                        >
+                            <View style={[styles.imageBox, pressedStates[5] && styles.imageBoxPressed]}>
+                                <Image source={Vector} />
+                            </View>
+                            <Text style={[styles.lawText, pressedStates[5] && styles.lawTextPressed]}>Languages</Text>
                     </Pressable>
                 </View>
             </View>
             <View style={styles.boxMain}>
-                    <Pressable style={[styles.box1, isPressed && styles.box1Pressed ]}
-                        onPressIn={()=> setIsPressed(true)}
-                        onPressOut={()=>setIsPressed(false)}
+                    <Pressable 
+                        style={[styles.box1, pressedStates[6] && styles.box1Pressed]}
+                        onPressIn={() => handlePressIn(6)}
+                        onPressOut={() => handlePressOut(6)}
                         >
-                            <View style={[styles.imageBox, isPressed && styles.imageBoxPressed]}>
+                            <View style={[styles.imageBox, pressedStates[6] && styles.imageBoxPressed]}>
                                 <Image source={Vector} />
                             </View>
-                            <Text style={[styles.lawText, isPressed && styles.lawTextPressed]}>Geography</Text>
+                            <Text style={[styles.lawText, pressedStates[5] && styles.lawTextPressed]}>Geography</Text>
                     </Pressable>
-                    <Pressable style={[styles.box1, isPressed && styles.box1Pressed ]}
-                        onPressIn={()=> setIsPressed(true)}
-                        onPressOut={()=>setIsPressed(false)}
+                    <Pressable 
+                        style={[styles.box1, pressedStates[7] && styles.box1Pressed]}
+                        onPressIn={() => handlePressIn(7)}
+                        onPressOut={() => handlePressOut(7)}
                         >
-                            <View style={[styles.imageBox, isPressed && styles.imageBoxPressed]}>
+                            <View style={[styles.imageBox, pressedStates[7] && styles.imageBoxPressed]}>
                                 <Image source={Vector} />
                             </View>
-                            <Text style={[styles.lawText, isPressed && styles.lawTextPressed]}>Computer Science</Text>
+                            <Text style={[styles.lawText, pressedStates[7] && styles.lawTextPressed]}>Computer Science</Text>
                     </Pressable>
                 </View>
         </View>

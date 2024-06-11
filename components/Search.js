3fcd4a5d32af371dbc1712adaecb850e938271db
Joseph <acheampongjoseph470@gmail.com>
2024-06-11 17:56:34 +0000

    updated some screen and components

--- components/Search.js
+++ components/Search.js
@@ -1,19 +1,21 @@
-import { Text, View, StyleSheet, TextInput } from 'react-native'
+import { Text, View, StyleSheet, TextInput, SafeAreaView } from 'react-native'
 import { EvilIcons } from '@expo/vector-icons';
 
 export default function Search(){
     return(
-        <View style={styles.container}>
-            <View style={styles.searchInput}>
-                <TextInput placeholder='Book Title' style={styles.input}/>
-                <EvilIcons name="search" size={24} color="black" style={{marginTop: -35, marginLeft: 10}}/>
+        <SafeAreaView>
+            <View style={styles.container}>
+                <View style={styles.searchInput}>
+                    <TextInput placeholder='Book Title' style={styles.input}/>
+                    <EvilIcons name="search" size={24} color="black" style={{marginTop: -35, marginLeft: 10}}/>
+                </View>
             </View>
-        </View>
+        </SafeAreaView>
     )
 }
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 54,
+        paddingTop: 30,
         paddingLeft: 40
     },
     
